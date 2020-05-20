b8090b76eb5c2f9ea7c010a32ca505051dae6793
unknown <anshnv@ya.ru>
2020-05-20 12:23:03 +0300

    field-date не установлен min

---
+++ src/lib/fields/field-text_date/_mix-field-date_with-button.js
@@ -0,0 +1,7 @@
+$(document).ready( function() {
+    /* var now = new Date();
+    var today = now.getDate()  + '/' + (now.getMonth() + 1) + '/' + now.getFullYear();
+    alert(today);
+    $('#datePicker').val(today); */
+    document.getElementById('datePicker').valueAsDate = new Date();
+});​
---
+++ src/pages/UlKit/formElements.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+    /* var now = new Date();
+    var today = now.getDate()  + '/' + (now.getMonth() + 1) + '/' + now.getFullYear();
+    alert(today);
+    $('#datePicker').val(today); */
+    document.getElementById('datePicker').valueAsDate = new Date.now();
+    document.getElementById('datePicker').valueAsDate = new $.now();
+});​
