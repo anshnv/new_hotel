39d570e0f2fff545e974c395b4efb411b5f22861
unknown <anshnv@ya.ru>
2020-05-25 18:41:30 +0300

    Поле с календарем с выбором дат завершено

--- src/lib/fields/calendar/calendar.js
+++ src/lib/fields/calendar/calendar.js
@@ -1,7 +1,23 @@
 import 'air-datepicker/dist/js/datepicker.min.js';
 import 'air-datepicker/dist/css/datepicker.min.css';
 
-$('#minMaxExample').datepicker({
+$(function(){
+	var date = new Date();
+	date.setDate(date.getDate());
+ 
+	$('#my-calendar').datepicker({
+		minDate: date
+	}); 
+});
+$(function(){
+ $('#my-calendar').datepicker({
     // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня
-    minDate: new Date()
+     // minDate: new Date()
+     // startDate: new Date()
+
 })
+})
+
+$(function(){
+ $('#my-calendar')._clearDate(this)})
+ 
