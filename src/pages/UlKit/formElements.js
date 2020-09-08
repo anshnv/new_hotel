83a05ede3da13e6c19c15d1625a56512449fcd8e
unknown <anshnv@ya.ru>
2020-09-08 21:32:56 +0300

    Вторая переделка БЭМ: input_text  input_date_short

--- src/pages/UlKit/formElements.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {
-    /* var now = new Date();
-    var today = now.getDate()  + '/' + (now.getMonth() + 1) + '/' + now.getFullYear();
-    alert(today);
-    $('#datePicker').val(today); */
-    document.getElementById('datePicker').valueAsDate = new Date.now();
-    document.getElementById('datePicker').valueAsDate = new $.now();
-});​
